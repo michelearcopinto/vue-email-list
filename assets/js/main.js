ebd8aea352df9ee56396e6e1f182266e839d9121
michelearcopinto <guestphantom1@gmail.com>
2024-02-05 16:57:30 +0100

    feat: method for mail creation

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,29 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-
+            randomMails: []
         }
     },
     created() {
 
     },
     methods: {
+        requestRandomMail() {
 
+            if (this.randomMails.length !== 0) {
+
+                this.randomMails = [];
+            }
+
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    let randomMail = result.data.response;
+
+                    this.randomMails.push(randomMail);
+                    console.log(this.indexMails)
+                })
+            }
+            console.log(this.randomMails)
+        }
     }
 }).mount('#app')
